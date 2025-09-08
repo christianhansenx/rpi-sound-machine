cdd04fd2681da975546748b9b39a69a433761a01
christianhansenx <67972214+christianhansenx@users.noreply.github.com>
2025-09-08 14:26:32 +0200

    250908 1426

--- rpi_sound_machine/start-tmux.sh
+++ rpi_sound_machine/start-tmux.sh
@@ -4,4 +4,5 @@ TMUX_SESSION_NAME="sound"
 cd ~/rpi_sound_machine
 tmux kill-session -t "$TMUX_SESSION_NAME" 2>/dev/null
 rm /tmp/rpi_sound_machine.tmux-log 2>/dev/null
-tmux new-session -d -s "$TMUX_SESSION_NAME"; tmux pipe-pane -t "$TMUX_SESSION_NAME":0.0 -o "cat >> /tmp/rpi_sound_machine.tmux-log"
+tmux new-session -d -s "$TMUX_SESSION_NAME"; \
+  tmux pipe-pane -t "$TMUX_SESSION_NAME":0.0 -o "cat >> /tmp/rpi_sound_machine.tmux-log"
